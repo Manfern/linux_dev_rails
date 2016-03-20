2cfa72a81c60e6f0acde35c48a10409a480bb214
Manfern <ejilur@gmail.com>
2016-03-21 02:19:33 +0300

    Added edit view, edited pictured, added edit link to index page and had problem with that

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,51 @@
 class PostsController < ApplicationController
-	def index
-		@posts=Post.all
-	end
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+	
+
+	def index
+		@posts=Post.all.order("created_at DESC")
+	end
+	def update
+		respond_to do |format|
+			if @post.update(post_params)
+
+				format.html {redirect_to(@post, nice: "Новость успешно отредактирована")}
+				format.json {head :no_content}
+			else
+				format.html {render action: "edit"}
+				format.json {render json: @post.errors, status: :unprocessable_entity}
+			end
+		end
+
+	end
 	def create
   		@post=Post.new(post_params)
 		@post.save
+
 		redirect_to @post
 	end
-	
+	def edit
+		@post = Post.new(params[:id])
+	end
+
+	def new
+		
+	end
+	def show
+		@post = Post.new(params[:id])
+	end
 
 private
 
 # Use strong_parameters for attribute whitelisting
 # Be sure to update your create() and update() controller methods.
-
-def post_params
-  params.require(:post).permit(:title, :content, :image)
-end
+	def set_post
+		@post = Post.find(params[:id])
+	end
+	
+	def post_params
+	  params.require(:post).permit(:title, :content, :image)
+	end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: { large:"415x230",medium: "200x200#", thumb: "135x135#" }, default_url: "/images/:style/missing.png"
+	validates :title, presence: true
+	validates :content, presence: true
+	
+	has_attached_file :image, styles: { large:"415x230",medium: "200x200#",small: "175x175#" ,thumb: "112x112#" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
